2534703ea85dbfaa60d10d18a84c1b992efa5560
Ayanna Soleil <ayanna.soleil92@gmail.com>
2021-12-08 15:43:42 -0500

    commit tree 40a110ad4f43ef11f43cbbc470bc02b4bef3dcd2

---
+++ index.js
@@ -0,0 +1,15 @@
+let num1 = 100
+let num2 = 2000
+const divide = function() {
+    return num2/num1;
+}
+divide();
+
+const square = (x) => x * x;
+square(x);
+
+function add (num3,num4) {
+    return num3 + num4;
+}
+
+add();
